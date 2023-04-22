d56ba05a6c590de7ac86e1704e674cb00b38f8dc
Marcos Kuyoc <marcos.kuyoc.dev@gmail.com>
2023-04-22 16:14:19 -0600

    feat: se agrega el titulo de la pagina

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,20 @@
 import { Content, Navbar } from '@/components';
-
+import Head from 'next/head';
 
 export default function Home() {
   return (
-    <main className="h-screen flex flex-col md:flex-row">
-      <section className="md:w-1/4">
-        <Navbar />
-      </section>
-      <section className="scroll-smooth w-full h-screen bg-nav pl-10 pr-10 overflow-y-auto">
-        <Content />
-      </section>
-    </main>
+    <>
+      <Head>
+        <title>Marcos Kuyoc</title>
+      </Head>
+      <main className="h-screen flex flex-col md:flex-row">
+        <section className="md:w-1/4">
+          <Navbar />
+        </section>
+        <section className="scroll-smooth w-full h-screen bg-nav pl-10 pr-10 overflow-y-auto">
+          <Content />
+        </section>
+      </main>
+    </>
   )
 }
